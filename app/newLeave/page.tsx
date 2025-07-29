35409521e25f832ab02de2052911eb6c6bd056d8
somoUi <alshykhb195@gmail.com>
2025-07-30 01:18:06 +0300

    m61

--- app/newLeave/page.tsx
+++ app/newLeave/page.tsx
@@ -307,7 +307,7 @@ function MainContent() {
     if (!time) return "";
     
     const [hours, minutes] = time.split(':').map(Number);
-    const period = hours >= 12 ? 'مساءً' : 'صباحاً';
+    const period = hours >= 12 ? 'PM' : 'AM';
     const hours12 = hours % 12 || 12;
     
     return `${hours12}:${minutes.toString().padStart(2, '0')} ${period}`;
