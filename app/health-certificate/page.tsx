273fc6a14b402ae7f2830905075aa55366fc9d6a
somoUi <alshykhb195@gmail.com>
2025-08-10 09:25:19 +0300

    m77

--- app/health-certificate/page.tsx
+++ app/health-certificate/page.tsx
@@ -5,7 +5,6 @@ export default function HealthCertificatePage() {
   return (
     <div className="flex flex-col items-center justify-start min-h-screen p-0 m-0 relative">
       {/* الصورة داخل حاوية نسبية */}
-      <div className="relative w-full" style={{ maxWidth: "2048px" }}>
         <Image
           src="/tr1.png"
           alt="الشهادة الصحية"
@@ -15,34 +14,32 @@ export default function HealthCertificatePage() {
           className="object-contain w-full h-auto"
         />
 
-        {/* المربعات النصية بالنسبة المئوية */}
-       <div
-  className="absolute text-black"
-  style={{
-    left: "85.0%",
-    top: "41%",
-    width: "24.41%",
-    height: "3.9%",
-    fontSize: "28px", // هنا تحدد الحجم بالبكسل
-    fontWeight: "bold" // إذا أردت يكون عريض
-  }}
->
-  <span>1234567890</span>
-</div>
-
+   {/* صورة الشخص مكان المربع الأخضر */}
         <div
-  className="absolute text-black"
-  style={{
-    left: "82.0%",
-    top: "55%",
-    width: "24.41%",
-    height: "3.9%",
-    fontSize: "28px", // هنا تحدد الحجم بالبكسل
-    fontWeight: "bold" // إذا أردت يكون عريض
-  }}
->
-  <span>1234567890123</span>
-</div>
+          className="absolute overflow-hidden"
+          style={{
+            left: "2.9%",
+            top: "8.3%",
+            width: "18.5%",
+            height: "11.8%"
+          }}
+        >
+          <Image
+            src="/face.jpg" // ضع مسار الصورة هنا
+            alt="صورة الشخص"
+            fill
+            className="object-cover"
+          />
+        </div>
+     
+    
+        {/* المربعات النصية بالنسبة المئوية */}
+       <div className="absolute text-black" style={{ left: "70.83%", top: "12.47%", width: "24.41%", height: "3.9%" }}>
+          <span>1234567890</span>
+        </div>
+        <div className="absolute text-black" style={{ left: "46.38%", top: "12.47%", width: "19.53%", height: "3.9%" }}>
+          <span>Indian</span>
+        </div>
         <div className="absolute text-black" style={{ left: "70.83%", top: "21.04%", width: "24.41%", height: "3.9%" }}>
           <span>HC-987654</span>
         </div>
@@ -62,6 +59,6 @@ export default function HealthCertificatePage() {
           <span>2026-06-01</span>
         </div>
       </div>
-    </div>
+   
   );
 }
