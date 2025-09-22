d674a893d3bb4269a2fef725f334384836e52fc3
feedbacklaila-cell <feedback.laila@gmail.com>
2025-09-23 02:48:07 +0300

    upda2

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8yhQzLkKdZT9xWxDhstPYtTIaOwMBL-8",
-  authDomain: "medical-certificate-9deab.firebaseapp.com",
-  projectId: "medical-certificate-9deab",
-  storageBucket: "medical-certificate-9deab.firebasestorage.app",
-  messagingSenderId: "803463932988",
-  appId: "1:803463932988:web:f1fe69d8dab36b8d7aa739",
-  measurementId: "G-PT6ZVG7F8T",
+  apiKey: "AIzaSyDYkn0D_dgYBjC3TxBVVWidJHkQyH4Hdm0",
+  authDomain: "medical-62016.firebaseapp.com",
+  projectId: "medical-62016",
+  storageBucket: "medical-62016.firebasestorage.app",
+  messagingSenderId: "488006149686",
+  appId: "1:488006149686:web:869d37346907e857eada97",
+  measurementId: "G-4GV2LHHT8H"
 };
 
 const app = initializeApp(firebaseConfig);
