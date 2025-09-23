32dc135511c5d06a041eb50b2b2754b3b084a451
feedbacklaila-cell <feedback.laila@gmail.com>
2025-09-23 03:42:43 +0300

    upda7

--- app/sa/Eservices/HealthIssue/PrintedLicenses/page.tsx
+++ app/sa/Eservices/HealthIssue/PrintedLicenses/page.tsx
@@ -541,7 +541,7 @@ return (
   src="/sher2.png"      // لاحظ / لأنها داخل public
   alt="icon"
   width={8}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
-  height={6}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
+  height={7}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
   className="mr-1 cursor-pointer"
 />
                     )}
@@ -564,8 +564,8 @@ return (
                        <Image
   src="/sher2.png"      // لاحظ / لأنها داخل public
   alt="icon"
-  width={5}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
-  height={4}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
+  width={8}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
+  height={7}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
   className="mr-1 cursor-pointer"
 />
                       </div>
@@ -661,8 +661,8 @@ return (
             <Image
   src="/sher2.png"      // لاحظ / لأنها داخل public
   alt="icon"
-   width={5}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
-  height={4}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
+   width={8}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
+  height={7}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
   className="mr-1 cursor-pointer"
 />
           )}
@@ -712,8 +712,8 @@ return (
                <Image
   src="/sher2.png"      // لاحظ / لأنها داخل public
   alt="icon"
-  width={5}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
-  height={4}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
+ width={8}            // بدل w-3.1 (تقريبًا 0.77rem × 64px = 49px)
+  height={7}           // بدل h-2.5 (تقريبًا 0.625rem × 64px = 40px)
   className="mr-1 cursor-pointer"
 />
               </div>
